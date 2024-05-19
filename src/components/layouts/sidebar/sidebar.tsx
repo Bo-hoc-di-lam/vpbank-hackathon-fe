cf5c332da6f59ded3a4901d3c57fcd74bb816e9e
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-05-19 16:16:19 +0700

    fix: sidebar items

--- src/components/layouts/sidebar/sidebar.tsx
+++ src/components/layouts/sidebar/sidebar.tsx
@@ -1,29 +1,50 @@
-import { ActionIcon, Stack } from "@mantine/core"
+import { ActionIcon, Stack, Tooltip } from "@mantine/core"
 import { Icon12Hours, IconAccessPoint } from "@tabler/icons-react"
 import useSidebarDrawer from "../../../hooks/use-sidebar-drawer"
+import { useMemo } from "react"
+
+interface SidebarItem {
+    icon: React.ReactNode
+    label: string
+    chilren: React.ReactNode
+}
 
 const Sidebar = () => {
+    const sidebarItems = useMemo<SidebarItem[]>(() => {
+        return [
+            {
+                icon: <Icon12Hours size={24} />,
+                label: "Time",
+                chilren: <div>Time</div>,
+            },
+            {
+                icon: <IconAccessPoint size={24} />,
+                label: "Network",
+                chilren: <div>Network</div>,
+            },
+        ]
+    }, [])
+
     const sidebarDrawer = useSidebarDrawer()
     return (
         <Stack gap={16}>
-            <ActionIcon
-                onClick={() =>
-                    sidebarDrawer.show({
-                        children: <div>This is sidebar</div>,
-                    })
-                }
-            >
-                <IconAccessPoint />
-            </ActionIcon>
-            <ActionIcon
-                onClick={() =>
-                    sidebarDrawer.show({
-                        children: <div>Phuong Linh xinh gai</div>,
-                    })
-                }
-            >
-                <Icon12Hours />
-            </ActionIcon>
+            {sidebarItems.map((item, index) => (
+                <Tooltip
+                    key={index}
+                    label={item.label}
+                    position="right"
+                    withArrow
+                >
+                    <ActionIcon
+                        onClick={() =>
+                            sidebarDrawer.show({ children: item.chilren })
+                        }
+                        aria-label={item.label}
+                    >
+                        {item.icon}
+                    </ActionIcon>
+                </Tooltip>
+            ))}
         </Stack>
     )
 }
