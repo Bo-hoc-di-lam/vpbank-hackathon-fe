80128086ecd65f2d8bbae4cdd29bd1fc83de3637
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-05-28 18:25:46 +0700

    fix: view mermaid btn

--- src/components/layouts/sidebar/sidebar-items/view-mermaid.tsx
+++ src/components/layouts/sidebar/sidebar-items/view-mermaid.tsx
@@ -1,29 +1,29 @@
-import { useDiagramManager } from '@/store/digaram-mananger-store';
+import { useDiagramManager } from "@/store/digaram-mananger-store"
 import { useClipboard, useDisclosure } from "@mantine/hooks"
-import { useState } from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import toast from 'react-hot-toast';
-import { Button, Modal } from "@mantine/core";
+import { useState } from "react"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism"
+import toast from "react-hot-toast"
+import { Button, Modal } from "@mantine/core"
 
-import { exportToMermaidFile } from '@/utils/file';
-import { IconCopy, IconFileExport } from '@tabler/icons-react';
+import { exportToMermaidFile } from "@/utils/file"
+import { IconCopy, IconFileExport } from "@tabler/icons-react"
 
 const ViewMermaid = () => {
-    const diagramManager = useDiagramManager();
-    const [codeString, setCodeString] = useState<string>(diagramManager.mermaid);
-    const [modalOpened, { open, close }] = useDisclosure(false);
+    const diagramManager = useDiagramManager()
+    const [codeString, setCodeString] = useState<string>(diagramManager.mermaid)
+    const [modalOpened, { open, close }] = useDisclosure(false)
 
     diagramManager.onMermaid((mermaid) => {
-        setCodeString(mermaid);
-    });
+        setCodeString(mermaid)
+    })
 
     const clipboard = useClipboard({ timeout: 500 })
 
     const copyToClipboard = () => {
         if (!codeString) {
-            toast.error('No code to copy');
-            return;
+            toast.error("No code to copy")
+            return
         }
 
         clipboard.copy(codeString)
@@ -32,22 +32,27 @@ const ViewMermaid = () => {
             return
         }
         toast.success("Copied to clipboard")
-    };
+    }
 
     const exportToFile = () => {
         if (!codeString) {
-            toast.error('No code to export');
-            return;
+            toast.error("No code to export")
+            return
         }
 
-        exportToMermaidFile(codeString);
-        toast.success('File exported successfully');
-    };
+        exportToMermaidFile(codeString)
+        toast.success("File exported successfully")
+    }
 
     return (
         <div className="flex flex-col gap-4">
-            <Modal opened={modalOpened} onClose={close} title="Mermaid" size="100%">
-                <div className='flex gap-4'>
+            <Modal
+                opened={modalOpened}
+                onClose={close}
+                title="Mermaid"
+                size="100%"
+            >
+                <div className="flex gap-4">
                     <Button
                         variant="light"
                         onClick={copyToClipboard}
@@ -65,25 +70,32 @@ const ViewMermaid = () => {
                         <IconFileExport size={16} />
                     </Button>
                 </div>
-                <SyntaxHighlighter language="plaintext" style={dracula} wrapLines={true}
-                    lineProps={{ style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' } }}>
-
+                <SyntaxHighlighter
+                    language="plaintext"
+                    style={dracula}
+                    wrapLines={true}
+                    lineProps={{
+                        style: {
+                            wordBreak: "break-all",
+                            whiteSpace: "pre-wrap",
+                        },
+                    }}
+                >
                     {codeString}
                 </SyntaxHighlighter>
-
-
             </Modal>
             <Button
                 fullWidth
-                variant='light'
+                variant="light"
                 onClick={open}
-            // disabled={codeString === ""}
+                disabled={codeString === ""}
             >
-                View
+                {codeString === ""
+                    ? "Please generate first to view"
+                    : "View Mermaid Code"}
             </Button>
-
         </div>
-    );
-};
+    )
+}
 
-export default ViewMermaid;
+export default ViewMermaid
