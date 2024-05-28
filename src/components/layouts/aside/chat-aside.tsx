7ad0ee090e9a304169d7a20bdf4f84dc0ee5d47e
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-05-28 19:48:54 +0700

    fix: chat color

--- src/components/layouts/aside/chat-aside.tsx
+++ src/components/layouts/aside/chat-aside.tsx
@@ -171,16 +171,16 @@ const ChatUi = ({ role, message }: Message) => {
                     </div>
                 </div>
                 <div className="chat-header">Chatbot</div>
-                <div className="chat-bubble chat-bubble-info flex items-center">
+                <div className="chat-bubble chat-bubble-info flex items-center bg-[#228be6]">
                     {message === "l" ? (
                         // Add loading dots animation here using tailwindcss
                         <div className="flex space-x-1">
-                            <div className="h-2 w-2 bg-gray-600 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-                            <div className="h-2 w-2 bg-gray-600 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-                            <div className="h-2 w-2 bg-gray-600 rounded-full animate-bounce delay-300"></div>
+                            <div className="h-2 w-2 bg-gray-100 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+                            <div className="h-2 w-2 bg-gray-100 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+                            <div className="h-2 w-2 bg-gray-100 rounded-full animate-bounce delay-300"></div>
                         </div>
                     ) : (
-                        <span>
+                        <span className="text-white">
                             <ReactMarkdown>{message}</ReactMarkdown>
                         </span>
                     )}
@@ -191,7 +191,9 @@ const ChatUi = ({ role, message }: Message) => {
         return (
             <div className="chat chat-end pr-4">
                 <div className="chat-header">You</div>
-                <div className="chat-bubble chat-bubble-info">{message}</div>
+                <div className="chat-bubble chat-bubble-info bg-[#228be6] text-white">
+                    {message}
+                </div>
             </div>
         )
     }
